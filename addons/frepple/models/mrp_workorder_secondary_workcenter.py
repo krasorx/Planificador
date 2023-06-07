85b06c09a67936c101f872866a82d6f5afbb19ba
Luis <40400309+krasorx@users.noreply.github.com>
2023-06-07 19:42:50 -0300

    Add files via upload
    Agregado app frepple

---
+++ addons/frepple/models/mrp_workorder_secondary_workcenter.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 by frePPLe bv
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from odoo import models, fields, api
+
+
+class WorkorderSecondaryWorkcenter(models.Model):
+    _name = "mrp.workorder.secondary.workcenter"
+    _description = "Secondary workcenter of a work order"
+    _rec_name = "workcenter_id"
+
+    workorder_id = fields.Many2one(
+        "mrp.workorder",
+        "Parent work order",
+        index=True,
+        ondelete="cascade",
+        required=True,
+    )
+    workcenter_id = fields.Many2one(
+        "mrp.workcenter",
+        "Work Center",
+        required=True,
+        ondelete="cascade",
+    )
+    duration = fields.Float("Duration", help="time in minutes")
